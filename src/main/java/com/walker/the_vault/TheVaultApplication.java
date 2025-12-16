d232debed3e383e7fa5de26fdd8b93629481ba3e
Justin Walker <justinwalker.contact@gmail.com>
2025-12-16 15:56:13 -0800

    feat: Add BaseEntity and User models

--- src/main/java/com/walker/the_vault/TheVaultApplication.java
+++ src/main/java/com/walker/the_vault/TheVaultApplication.java
@@ -2,8 +2,10 @@ package com.walker.the_vault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TheVaultApplication {
 
 	public static void main(String[] args) {
