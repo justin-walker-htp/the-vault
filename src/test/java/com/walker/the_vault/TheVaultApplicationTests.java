a6840ba3765011c726c43ccc2c48a893a9c02872
Justin Walker <justinwalker.contact@gmail.com>
2025-12-16 04:27:37 -0800

    feat: Initial commit.

---
+++ src/test/java/com/walker/the_vault/TheVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.the_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
